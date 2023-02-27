53fd74e5569b876ff0675cd1ac3168c87acf5602
Willy-SDK <kowit.will@gmail.com>
2023-02-27 16:13:33 +0700

    Add CitySearch ViewController

---
+++ CitySearch/CitySearch/Utility/Application/Application.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+final class Application {
+    static let shared = Application()
+    
+    func configureMainInterface(in window: UIWindow) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let searchCityNavigationController = UINavigationController()
+        
+    }
+    
+}
