09d8214cb7708dc1026cae1ca0c56f1fd1aa0f90
Willy-SDK <kowit.will@gmail.com>
2023-02-27 16:18:11 +0700

    add Default CitySearch Navigator

---
+++ CitySearch/CitySearch/Utility/Scenes/CitySearch/DefaultCitySearchNavigator.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class DefaultCitySearchNavigator {
+    private let storyBoard: UIStoryboard
+    private let navigationController: UINavigationController
+    
+    init(storyBoard: UIStoryboard, navigationController: UINavigationController) {
+        self.storyBoard = storyBoard
+        self.navigationController = navigationController
+    }
+    
+    func toCitySearch() {
+        
+    }
+}
