a87da6f8554e9fb312a005e9cce33064697a7ddf
Willy-SDK <kowit.will@gmail.com>
2023-03-01 18:21:06 +0700

    Implement Map ViewModel

--- CitySearch/CitySearch/Scenes/Map/MapViewModel.swift
+++ CitySearch/CitySearch/Scenes/Map/MapViewModel.swift
@@ -1,8 +1,32 @@
-//
-//  MapViewModel.swift
-//  CitySearch
-//
-//  Created by Willy on 28/02/2023.
-//
 
-import Foundation
+import Domain
+import RxSwift
+import RxCocoa
+
+final class MapViewModel {
+    private let cities: Cities
+    private let navigator: MapNavigator
+    
+    init(cities: Cities, navigator: MapNavigator) {
+        self.cities = cities
+        self.navigator = navigator
+    }
+    
+    func transform(input: Input) -> Output {
+        let cities = input.trigger
+            .map { self.cities }
+        
+        return Output(cities: cities)
+    }
+    
+}
+
+extension MapViewModel {
+    struct Input {
+        let trigger: Driver<Void>
+    }
+
+    struct Output {
+        let cities: Driver<Cities>
+    }
+}
