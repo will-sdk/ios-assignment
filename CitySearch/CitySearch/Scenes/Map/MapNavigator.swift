b0f2e9b7f666171a30357dc3d99dba77816769af
Willy-SDK <kowit.will@gmail.com>
2023-03-01 18:21:37 +0700

    Implement Map Navigator

--- CitySearch/CitySearch/Scenes/Map/MapNavigator.swift
+++ CitySearch/CitySearch/Scenes/Map/MapNavigator.swift
@@ -1,8 +1,18 @@
-//
-//  MapNavigator.swift
-//  CitySearch
-//
-//  Created by Willy on 28/02/2023.
-//
 
-import Foundation
+import UIKit
+
+protocol MapNavigator {
+    func toPosts()
+}
+
+final class DefaultMapNavigator: MapNavigator {
+    private let navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func toPosts() {
+        navigationController.popViewController(animated: true)
+    }
+}
