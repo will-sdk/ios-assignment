9a3acfedf51e264a6d971fa811053e07531accf8
Willy-SDK <kowit.will@gmail.com>
2023-03-01 18:23:06 +0700

    Implement Mapkit and Mapping data form Cities Model

--- CitySearch/CitySearch/Scenes/Map/MapViewController.swift
+++ CitySearch/CitySearch/Scenes/Map/MapViewController.swift
@@ -1,29 +1,65 @@
-//
-//  MapViewController.swift
-//  CitySearch
-//
-//  Created by Willy on 28/02/2023.
-//
+
 
 import UIKit
+import MapKit
+import SnapKit
+import Domain
+import RxSwift
+
+struct MapViewConstants {
+    static let coordinate: Double = 1000
+}
 
 class MapViewController: UIViewController {
 
+    var viewModel: MapViewModel!
+    
+    let disposBag = DisposeBag()
+    let mapView = MKMapView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+        bindElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupViews() {
+        // Add map view to the view
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
-    */
-
+    
+    private func bindElements() {
+        let viewWillAppear = rx.sentMessage(#selector(UIViewController.viewWillAppear(_:)))
+            .mapToVoid()
+            .asDriverOnErrorJustComplete()
+        
+        let input = MapViewModel.Input(trigger: viewWillAppear)
+        let output = viewModel.transform(input: input)
+        
+        output.cities.drive(cityBinding)
+            .disposed(by: disposBag)
+    }
+    
+    var cityBinding: Binder<Cities> {
+        return Binder(self, binding: { (_ , city) in
+            self.setupMapViewBy(lat: city.coord.lat, long: city.coord.lon)
+        })
+    }
+    
+    private func setupMapViewBy(lat: Double, long: Double) {
+        // Set map region and add a pin
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: MapViewConstants.coordinate, longitudinalMeters: MapViewConstants.coordinate)
+        mapView.setRegion(region, animated: false)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+    
 }
+
+
