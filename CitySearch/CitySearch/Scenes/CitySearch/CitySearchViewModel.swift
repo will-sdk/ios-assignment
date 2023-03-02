de306ec9895e8948c5cf64e990562bacc9a29879
Willy-SDK <kowit.will@gmail.com>
2023-03-02 15:01:13 +0700

    Implement Filter the cities based on the search query

--- CitySearch/CitySearch/Scenes/CitySearch/CitySearchViewModel.swift
+++ CitySearch/CitySearch/Scenes/CitySearch/CitySearchViewModel.swift
@@ -9,6 +9,7 @@ final class CitySearchViewModel {
     struct Input {
         let trigger: Driver<Void>
         let selection: Driver<IndexPath>
+        let searchQuery = BehaviorRelay<String>(value: "")
     }
     struct Output {
         let cities: Driver<[CitySearchItemViewModel]>
@@ -24,19 +25,32 @@ final class CitySearchViewModel {
     }
     
     func transform(input: Input) -> Output {
-        let cities = input.trigger.flatMapLatest {
+        let allCities = input.trigger.flatMapLatest {
             return self.usecase.listOfCities()
                 .asDriverOnErrorJustComplete()
                 .map { $0.map { CitySearchItemViewModel(with: $0) } }
         }
         
+        let searchQuery = input.searchQuery
+            .distinctUntilChanged()
+            .asDriverOnErrorJustComplete()
+        
+        let filteredCities = Driver.combineLatest(allCities, searchQuery)
+            .map { cities, query -> [CitySearchItemViewModel] in
+                if query.isEmpty {
+                    return cities
+                } else {
+                    return cities.filter { $0.cityName.localizedCaseInsensitiveContains(query) }
+                }
+        }
+        
         let selectedCity = input.selection
-            .withLatestFrom(cities) { (index, cities) -> Cities in
+            .withLatestFrom(filteredCities) { (index, cities) -> Cities in
                 return cities[index.row].cities
             }
             .do(onNext: navigator.toMapView)
-        
-        return Output(cities: cities,
-                      selectedCity: selectedCity)
-        }
+                
+                return Output(cities: filteredCities,
+                              selectedCity: selectedCity)
+    }
 }
