9033fada714b2115ab740ccc50082845325ee5a8
Willy-SDK <kowit.will@gmail.com>
2023-03-04 17:53:04 +0700

    Improve logic search use logic binary search

--- CitySearch/CitySearch/Scenes/CitySearch/CitySearchViewModel.swift
+++ CitySearch/CitySearch/Scenes/CitySearch/CitySearchViewModel.swift
@@ -3,6 +3,7 @@ import Foundation
 import RxSwift
 import RxCocoa
 import Domain
+import UIKit
 
 final class CitySearchViewModel {
     
@@ -13,11 +14,15 @@ final class CitySearchViewModel {
     }
     struct Output {
         let cities: Driver<[CitySearchItemViewModel]>
-        let selectedCity: Driver<Cities>
+        let selectedCity: Driver<CitySearchItemViewModel>
     }
     
     private let navigator: CitySearchNavigator
     private let usecase: ListOfCitiesUseCase
+    
+    var items = [CitySearchItemViewModel]()
+    private var itemsReversed = [CitySearchItemViewModel]()
+    var resultsIndex: (first: Int, last: Int) = (first: 0, last: 0)
     
     init(navigator: CitySearchNavigator, usecase: ListOfCitiesUseCase) {
         self.navigator = navigator
@@ -29,6 +34,9 @@ final class CitySearchViewModel {
             return self.usecase.listOfCities()
                 .asDriverOnErrorJustComplete()
                 .map { $0.map { CitySearchItemViewModel(with: $0) } }
+                .do {
+                    self.setupData(cities: $0)
+                }
         }
         
         let searchQuery = input.searchQuery
@@ -37,20 +45,108 @@ final class CitySearchViewModel {
         
         let filteredCities = Driver.combineLatest(allCities, searchQuery)
             .map { cities, query -> [CitySearchItemViewModel] in
-                if query.isEmpty {
-                    return cities
-                } else {
-                    return cities.filter { $0.fullNameOfCityAndCountry.lowercased().hasPrefix(query.lowercased()) }
-                }
-        }
+                return self.filter(search: query)
+            }
         
         let selectedCity = input.selection
-            .withLatestFrom(filteredCities) { (index, cities) -> Cities in
-                return cities[index.row].cities
+            .withLatestFrom(filteredCities) { (index, cities) -> CitySearchItemViewModel in
+                return self.itemWithOffset(offset: index.row)
             }
             .do(onNext: navigator.toMapView)
                 
-                return Output(cities: filteredCities,
-                              selectedCity: selectedCity)
+        return Output(cities: filteredCities,
+                    selectedCity: selectedCity)
+    }
+    
+    // Sets the value for all searchValue in the cities
+    private func setupData(cities: [CitySearchItemViewModel]) {
+        items = cities
+        parseSearchValues()
+        sortItems()
+        resultsIndex = (first: 0, last: items.count - 1)
+    }
+    
+    private func parseSearchValues() {
+        for index in 0..<items.count {
+            items[index].searchValue = items[index].cityAndCountry.lowercased()
+        }
+    }
+    
+    private func sortItems() {
+        items = items.sorted {
+            $0.searchValue < $1.searchValue
+        }
+        itemsReversed = items.reversed()
+    }
+    
+    // Find item with Offset
+    func itemWithOffset(offset: Int) -> CitySearchItemViewModel {
+        return items[resultsIndex.first + offset]
+    }
+    
+    // Filter Search Data return Array CitySearchItemViewModel
+    private func filter(search: String) -> [CitySearchItemViewModel] {
+        if search.isEmpty {
+            // If search is empty, all items are visible
+            resultsIndex = (first: 0, last: items.count - 1)
+            return items
+        }
+        else {
+            return findRange(search: search)
+        }
+    }
+    
+    private func findRange(search: String) -> [CitySearchItemViewModel] {
+        let startDate = Date()
+        let indexFirst = findFirstIndex(search: search)
+        let indexLast = findLastIndex(search: search)
+        
+        resultsIndex = (first: indexFirst, last: indexLast)
+        debugPrint("Query Text: \(search) - time: \(Date().timeIntervalSince(startDate))")
+        return items
+    }
+    
+    private func findFirstIndex(search: String) -> Int {
+        var start = 0
+        var end = items.count - 1
+        let search = search.lowercased()
+        
+        while start < end {
+            let mid = (start + end) / 2
+            
+            if items[mid].searchValue < search {
+                start = mid + 1
+            } else {
+                end = mid
+            }
+        }
+        
+        if items[start].searchValue.hasPrefix(search) {
+            return start
+        } else {
+            return end + 1
+        }
+    }
+    
+    private func findLastIndex(search: String) -> Int {
+        var start = 0
+        var end = itemsReversed.count - 1
+        let search = search.lowercased()
+        
+        while start < end {
+            let mid = (start + end) / 2
+            
+            if itemsReversed[mid].searchValue > search {
+                start = mid + 1
+            } else {
+                end = mid
+            }
+        }
+        
+        if itemsReversed[start].searchValue.hasPrefix(search) {
+            return items.count - 1 - start
+        } else {
+            return items.count - 1 - end - 1
+        }
     }
 }
